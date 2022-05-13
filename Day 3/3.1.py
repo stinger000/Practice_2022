08ead3807ec7169002ddce7a4636d26e4ed5f363
Sergey Stetsky <bigbani1334@gmail.com>
2022-05-13 11:14:54 +0300

    Add day 3 files

---
+++ Day 3/3.1.py
@@ -0,0 +1,21 @@
+# Ввод нескольких значений
+# string = inpunt() - считывает строку целиком
+
+# s = input("Введите координаты точки через пробел: ")
+# print(int(s))  # Если ввести два числа, возникнет ошибка
+
+# string.split() - разделяет строку
+
+string = "1 2"
+x, y = string.split()
+print(f"{x=}, {y=}")
+# print(x * y )  # Вызовет ошибку - нельзя перемножать строки
+x, y = float(x), float(y)
+print(x * y )  # Выполнится без ошибки
+
+# Другие символы разделителя
+date = "13.05.2022"
+day, month, year = date.split(".")
+day, month, year = int(day), int(month), int(year)
+print(f"Год: {year}, месяц: {month}, день: {day}")
+
