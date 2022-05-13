033e49ad1c8cd0fe28f63b48cf6309a0a26fac2c
Sergey Stetsky <bigbani1334@gmail.com>
2022-05-13 11:15:51 +0300

    Update README.md

--- Day 3/3.1.py
+++ Day 3/3.1.py
@@ -11,11 +11,10 @@ x, y = string.split()
 print(f"{x=}, {y=}")
 # print(x * y )  # Вызовет ошибку - нельзя перемножать строки
 x, y = float(x), float(y)
-print(x * y )  # Выполнится без ошибки
+print(x * y)  # Выполнится без ошибки
 
 # Другие символы разделителя
 date = "13.05.2022"
 day, month, year = date.split(".")
 day, month, year = int(day), int(month), int(year)
 print(f"Год: {year}, месяц: {month}, день: {day}")
-
