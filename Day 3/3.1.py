a103f4df3688f08e2d1bb9f002c7214eafe9648b
Sergey Stetsky <bigbani1334@gmail.com>
2022-05-13 11:54:06 +0300

    Update

--- Day 3/3.1.py
+++ Day 3/3.1.py
@@ -18,3 +18,32 @@ date = "13.05.2022"
 day, month, year = date.split(".")
 day, month, year = int(day), int(month), int(year)
 print(f"Год: {year}, месяц: {month}, день: {day}")
+
+# Быстрый способ считать несколько чисел
+# x, y = [int(x) for x in input().split()]
+# print(f"{x=}, {y=}")
+
+# Ещё один
+# x, y = map(int, input().split())
+# print(f"{x=}, {y=}")
+
+# Методы строк
+
+string = "Ооооочень длинная строка"
+print(f"Строка {string=} имеет длину {len(string)}")
+
+# Заменить подстроку в строке
+string = "c++ - лучший язык в мире"
+string = string.replace("c++", "Python")
+print(string)
+
+# Капс
+print(string.upper())
+
+
+print("-"*120)
+print("Прекрасная программа")
+print("Конец прекрасной программы".center(120, "-"))
+
+# Остальные методы строк
+# https://pythonworld.ru/tipy-dannyx-v-python/stroki-funkcii-i-metody-strok.html
