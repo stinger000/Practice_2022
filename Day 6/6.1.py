2136f8a7068791b0651c82c47b66d8cb19a476b3
Sergey Stetsky <bigbani1334@gmail.com>
2022-05-19 11:48:17 +0300

    Day 6 update

--- Day 6/6.1.py
+++ Day 6/6.1.py
@@ -13,3 +13,14 @@ print(f"{smartest_student=}")
 # Операция обращения по индексу - опасная
 # print(students[4])  # Выведет ошибку
 
+# Определение размера списка
+
+students_count = len(students) # Количество студентов (размер списка)
+
+# Вывод всех элементов из списка
+i = 0
+while i < students_count:
+    print(students[i])
+    i += 1
+
+
