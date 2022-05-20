bb676a2aff8c9bfd514c3b2d17213fb1f716986d
Sergey Stetsky <bigbani1334@gmail.com>
2022-05-20 13:15:35 +0300

    Day 7

---
+++ Day 7/7.2.py
@@ -0,0 +1,23 @@
+# Цикл for
+# for var in iterable:
+# var - переменная
+# iterable - то, что мы перебираем
+
+n = 10
+for i in range(n):
+    print(i)
+
+print('')
+
+start = 5
+stop = 10
+for i in range(start, stop):
+    print(i)
+
+print('')
+
+start = 5
+stop = 10
+step = 2
+for i in range(start, stop, step):
+    print(i)
