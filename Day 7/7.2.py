9b504e21a99e39c303e671845dab45eddce99ed6
Sergey Stetsky <bigbani1334@gmail.com>
2022-05-20 14:15:10 +0300

    Day 7

--- Day 7/7.2.py
+++ Day 7/7.2.py
@@ -3,9 +3,11 @@
 # var - переменная
 # iterable - то, что мы перебираем
 
+# range - создает диапазон чисел
 n = 10
 for i in range(n):
     print(i)
+
 
 print('')
 
@@ -21,3 +23,20 @@ stop = 10
 step = 2
 for i in range(start, stop, step):
     print(i)
+
+# цикл for можно проводить по любому итерируемому объекту
+
+students = ["Ананас", "Руслан", "Алексей", "Кокос"]
+
+for student in students:
+    print(f"{student} отчислен")
+
+print(students)
+
+for i in range(len(students)):
+    students[i] = ""
+
+print(students)
+
+
+
