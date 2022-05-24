635709b39f09f2ba2710a3f789d3048feaad04a9
Sergey Stetsky <bigbani1334@gmail.com>
2022-05-24 12:46:33 +0300

    Day 9

--- Day 9/9.1.py
+++ Day 9/9.1.py
@@ -10,7 +10,7 @@ def shout(wordlist):
         print(word.upper() + "!!!!!!1")
 
 
-shout(words)
+# shout(words)
 
 # Функции могут изменять содержимое списков
 
@@ -18,16 +18,43 @@ def modify(wordlist):
     wordlist.append("Hello, world!")
 
 modify(words)
-print(words)
+# print(words)
 
 def not_modify(wordlist): # Список не будет изменен
     wordlist = []
 
 not_modify(words)
-print(words)
+# print(words)
 
 def really_clear_the_list(wordlist): # Список будет изменен
     wordlist.clear()
 
 really_clear_the_list(words)
-print(words)
+# print(words)
+
+# Списки могут быть вложенными
+
+# Пример создания вложенного списка
+
+library = [
+    ["Гоголь", "Есенин", "Пушкин"], # 1 полка
+    ["Перышкин", "Эйнштейн"], # 2 полка
+    ["Бредберри", "Хоккинг"]  # 3 полка
+]
+print(library)
+
+# Допустим, мы хотим добавить полку к библиотеке
+library.append(["Стругацкие"])
+print(library)
+
+# Добавить книгу на существующую полку
+library[0].append("Толстой")
+print(library)
+
+i = 1
+for bookshelf in library:
+    print(f"На книжной полке {i} стоят следующие книги:")
+    for book in bookshelf:
+        print(book)
+    print("-"*80)
+    i += 1
