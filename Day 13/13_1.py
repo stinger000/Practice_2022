657c338e748b4d2459b7df2e437d3392f9aacff6
Sergey Stetsky <bigbani1334@gmail.com>
2022-06-03 11:23:07 +0300

    Day 13 task

--- Day 13/13_1.py
+++ Day 13/13_1.py
@@ -19,10 +19,59 @@ def get_point_from_keyboard() -> Point:
     return Point(x, y)
 
 
-p = get_point_from_keyboard()
-q = get_point_from_keyboard()
+class Triangle:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
 
-# print_point(p)
-print(p)
-print(q)
-print(f"Расстояние равно {p.distance_to(q)}")
+    def is_exist(self):
+        return self.a + self.b > self.c and \
+               self.a + self.c > self.b and \
+               self.b + self.c > self.a
+
+    def area(self):
+        if not self.is_exist():
+            return -1
+        p = (self.a + self.b + self.c) / 2
+        s = (p * (p - self.a) * (p - self.b) * (p - self.c)) ** 0.5
+        return s
+
+
+def make_triangle_from_points(p1: Point, p2: Point, p3: Point) -> Triangle:
+    a = p1.distance_to(p2)
+    b = p2.distance_to(p3)
+    c = p3.distance_to(p1)
+    return Triangle(a, b, c)
+
+
+def make_triangle_from_keyboard() -> Triangle:
+    """
+    Запрашивает у пользователя ввод координат,
+    и составляет треугольник с вершинами во
+    введенных координатах.
+    """
+    t = make_triangle_from_points(
+        get_point_from_keyboard(),
+        get_point_from_keyboard(),
+        get_point_from_keyboard()
+    )
+    return t
+
+
+def test_1():
+    triangles = []
+    for i in range(2):
+        triangles.append(make_triangle_from_keyboard())
+
+    for t in triangles:
+        if (area := t.area()) > 0:
+            print(area)
+        else:
+            print("Такого треугольника нет")
+
+
+
+
+if __name__ == '__main__':
+    test_1()
